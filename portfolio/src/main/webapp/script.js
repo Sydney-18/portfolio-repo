d4b0cd6599bb400042bb25d5abcd9f65d8146624
Sydney-18 <sydbeas2018@gmail.com>
2020-05-30 00:43:58 -0400

    Style the page, fix stying comments about tags, add quotes to the quote generator.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,11 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Act as if what you do makes a difference. It does. - William James', 'Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchhill', 
+       'Never bend your head. Always hold it high. Look the world straight in the eye. - Hellen Keller', 'It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome. - William James',
+       'Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein', 'Sometimes you will never know the value of a moment, until it becomes a memory. - Dr. Seus', 'The most wasted of days is one without laughter. - E. E. Cummings',
+       'You must do the things you think you cannot do. - Eleanor Roosevelt', 'It is never too late to be what you might have been. - George Eliot', 'Never limit yourself because of others’ limited imagination; never limit others because of your own limited imagination. - Mae Jemison',
+       'Be the change that you wish to see in the world. - Mahatma Gandhi', 'If I cannot do great things, I can do small things in a great way. - Martin Luther King Jr.', 'Each person must live their life as a model for others. - Rosa Parks'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
